b127ae3db13355b482e926f60518770dafa6d9f2
Tran Tam <tranthanhtam30499@gmail.com>
2021-10-10 22:21:10 +0700

    fix search

--- src/components/SearchInput/SearchBox.jsx
+++ src/components/SearchInput/SearchBox.jsx
@@ -35,11 +35,5 @@ const SearchBox = () => {
         </Box>
     )
 }
-const top100Films = [
-    { title: 'The Shawshank Redemption', year: 1994 },
-    { title: 'The Godfather', year: 1972 },
-    { title: 'The Godfather: Part II', year: 1974 },
-    { title: 'The Dark Knight', year: 2008 },
-    { title: '12 Angry Men', year: 1957 },
-]
+const top100Films = []
 export default SearchBox
