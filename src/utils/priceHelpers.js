9028796347b06ac7e5f4955fe686d5063c591a7e
jefftran99 <93501789+jefftran99@users.noreply.github.com>
2021-12-28 17:54:37 +0700

    update api

--- src/utils/priceHelpers.js
+++ src/utils/priceHelpers.js
@@ -5,7 +5,7 @@ import { getParameterCaseInsensitive } from 'utils/index';
 export async function getPrices() {
   const prices = {}
 
-  let res = await axios.get('https://api.info.kaidex.io/api/tokens')
+  let res = await axios.get('https://kai.kshark.io/api')
   for (const [key, v] of Object.entries(res.data.data)) {
     if (v.price) prices[key.toLowerCase()] = +v.price
   }
